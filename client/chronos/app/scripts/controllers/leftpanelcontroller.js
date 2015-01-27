9dc480fbfec8fb612c3c3d826a36e319ec3b8547
Justin Guze <jaguze@gmail.com>
2015-01-27 00:03:28 -0800

    Made the event cards handle more actual event info

--- client/chronos/app/scripts/controllers/leftpanelcontroller.js
+++ client/chronos/app/scripts/controllers/leftpanelcontroller.js
@@ -11,13 +11,29 @@
 angular.module('chronosApp')
   .controller('LeftPanelController', function ($scope, StateService, RestService) {
    	$scope.events = [{
-   		eventTitle: "Title Test",
-   		test: "TESTING",
    		eventId: "1",
+   		title: "Title Test",
+   		description: "A very basic description of an event",
+   		creator: "Carljr.",
+   		creationDate: "01/24/1992",
+   		editDate: "None",
+   		location: "Victoria, BC",
+   		startDate: "01/24/1992",
+   		endDate: "01/24/2020",
+   		vote: "10",
+   		tags: "???",
    	},
    	{
-   		eventTitle: "Second event",
-   		test: "New Test",
    		eventId: "2",
+   		title: "Some Place",
+   		description: "Another description of an event",
+   		creator: "Carlossenior",
+   		creationDate: "01/24/1992",
+   		editDate: "None",
+   		location: "Victoria, BC",
+   		startDate: "01/24/1992",
+   		endDate: "01/24/2020",
+   		vote: "12",
+   		tags: "Music",
    	},];
   });
