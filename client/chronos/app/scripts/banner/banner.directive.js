50f61c8102da8ee206c0a5524f6874ba6b56d3a1
Justin Guze <jaguze@gmail.com>
2015-03-04 04:41:16 -0800

    Automatic place service lookup on login for users

--- client/chronos/app/scripts/banner/banner.directive.js
+++ client/chronos/app/scripts/banner/banner.directive.js
@@ -10,10 +10,24 @@
 
   angular
     .module('chronosApp')
-    .directive('banner', function () {
-      return {
-        templateUrl: 'scripts/banner/banner.html',
-        restrict: 'E'
-      };
-    });
+    .directive('banner', banner);
+
+    function banner() {
+    	var directive = {
+    		templateUrl: 'scripts/banner/banner.html',
+    		restrict: 'E',
+    		link: link
+    	};
+    	return directive;
+
+    function link(scope, element, attrs) {
+    		// This is grabbing the #empty div in the HTML to use as a placeholder map 
+    		angular.forEach(element[0].childNodes, function(child) {
+    			if (child.className == "empty")
+    			{
+    				scope._element = child;
+    			}
+    		});
+    	}
+    }
 })();
