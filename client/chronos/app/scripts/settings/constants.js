44bf6dbd1b20674bb71cf617662cd73bd99a4974
MarkRoller <eng.mroller@gmail.com>
2015-03-26 13:22:41 -0700

    minor change before pull request

--- client/chronos/app/scripts/settings/constants.js
+++ client/chronos/app/scripts/settings/constants.js
@@ -10,7 +10,7 @@
   angular
     .module('chronosApp')
     .constant('settings', {
-      serverUrl: '@@serverUrl',
+      serverUrl: 'http://localhost:8000',
       maxKeywords: 10,
       maxTagLength: 50,
       maxNumberTags: 5,
